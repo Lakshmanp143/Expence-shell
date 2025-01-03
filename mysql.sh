0074f7e4b3ba05d2075032376078a38a92cf2b14
Lakshmanpagala <laxman.gdn@gmail.com>
2025-01-03 19:43:45 +0530

    Expense shell

--- mysql.sh
+++ mysql.sh
@@ -10,19 +10,19 @@ LOGS_FILE=$(echo $0 | cut -d "." -f1 )
 LOGS_FILE_NAME="$LOGS_FOLDER/$LOGS_FILE-$TIMESTAMP.lOG"
 
 VALIDATE(){
-if [ $1 -ne 0 ]; then
-    echo -e "$2..... $R FAILURE $N"
-    exit 1
-else
-    echo -e "$2..... $G SUCCESS $N"
-fi    
+    if [ $1 -ne 0 ]; then
+        echo -e "$2..... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2..... $G SUCCESS $N"
+    fi    
 }
 
 CHECK_ROOT(){
-if [ $? -ne 0 ]; then
-    echo " You need be root user to execute this script"
-    exit 1
-fi
+    if [ $? -ne 0 ]; then
+        echo " You need be root user to execute this script"
+        exit 1
+    fi
 }
 echo "Script started executing at: $TIMESTAMP " &>>$LOGS_FILE_NAME
 CHECK_ROOT  
@@ -39,8 +39,6 @@ VALIDATE $? "Starting mysqld service"
 #mysql -h mysql.lakshman.site -u root -pExpense@App1 -e 'show databases;' &>>$LOGS_FILE_NAME
 #if [ $? -ne o ]; then
 #   echo "mysql root password not setup" &>>$LOGS_FILE_NAME
-#else
-
 
 mysql_secure_installation --set -root -pass Expense@App1  &>>$LOGS_FILE_NAME
 VALIDATE $? "Setting root password"
