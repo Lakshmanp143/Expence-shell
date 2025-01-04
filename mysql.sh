08fa42e48b3f1d32ab0454470ac6e152be709419
Lakshmanpagala <laxman.gdn@gmail.com>
2025-01-04 10:20:29 +0530

    mysql file modified

--- mysql.sh
+++ mysql.sh
@@ -39,7 +39,8 @@ VALIDATE $? "Enabling mysqld serveice"
 systemctl start mysqld  &>>$LOGS_FILE_NAME
 VALIDATE $? "Starting mysqld service"
 
-sudo mysql -h mysql.lakshman.site -u root -pExpense@App1 -e 'show databases;' &>>$LOGS_FILE_NAME
+mysql -h mysql.lakshman.site -u root -pExpenseApp@1 -e 'show databases;' &>>$LOGS_FILE_NAME
+
 if [ $? -ne 0 ]
 then
     echo "mysql root password not setup" &>>$LOGS_FILE_NAME
